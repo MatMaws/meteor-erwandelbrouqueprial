ff442da1ed75d60a5472901d473aacb191836b8e
Delbrouque Prial Erwan <delbrouquepri@b13p10.fil.univ-lille1.fr>
2018-03-19 10:26:39 +0100

    first step done

--- imports/startup/client/useraccounts-configuration.js
+++ imports/startup/client/useraccounts-configuration.js
@@ -1,3 +1,4 @@
-import {Accounts} from 'meteor/accounts-base';
-
-// TODO : Injecter le code pour l'authent
+import { Accounts } from 'meteor/accounts-base';
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY', // un pseudo au lieu d'un email pour l'authentification
+});
--- imports/ui/components/navbar/index.js
+++ imports/ui/components/navbar/index.js
@@ -2,5 +2,9 @@ import './navbar.html';
 import './navbar.css';
 
 import { Template } from 'meteor/templating';
-
-// TODO: Ajouter les helpers
+Template.navbar.helpers({
+    dollarWallet() {
+      return 9876;
+    },
+    currency: 'dollars',
+  });
